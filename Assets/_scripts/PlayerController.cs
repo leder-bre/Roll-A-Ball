e125647adea4daa8109f64a7016297e413052cbf
rsgc-leder-b <bleder@rsgc.on.ca>
2016-02-23 18:10:41 -0500

    Roll-A-Ball
    Basic sketch used to learn Unity basics - Referenced from
    <https://unity3d.com/learn/tutorials/projects/roll-ball-tutorial>

---
+++ Assets/_scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+    public float speed;
+    private int count;
+    public Text countText;
+    public Text winText;
+    
+	void Start() {
+		rb = GetComponent<Rigidbody> ();
+        count = 0;
+        setCountText();
+        winText.text = "";
+	}
+
+	void Update() {
+
+	}
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0f, moveVertical);
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive(false);
+            count += 1;
+            setCountText();
+            if (count >= 12) {
+                winText.text = "You Win";
+            }
+		}
+	}
+    
+    void setCountText() {
+        countText.text = "Count: " + count.ToString();
+    }
+
+}
---
+++ Assets/_scripts/Rotator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+	Random rnd = new Random();
+	// Update is called once per frame
+	void Update () {
+	   transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+	   transform.Rotate (new Vector3 (Random.value * 10, Random.value * 10, Random.value * 10) * Time.deltaTime);
+	}
+}
